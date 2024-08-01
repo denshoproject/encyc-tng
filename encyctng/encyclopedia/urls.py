36a05332b348c0f6d7dded58e393dc587c4b3cff
Geoffrey Jost <geoffrey.jost@densho.org>
2024-08-01 13:29:50 -0700

    Add primary source detail pages, link from streamfieldblocks

--- encyctng/encyclopedia/urls.py
+++ encyctng/encyclopedia/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('authors/', views.authors, name='encyc-authors'),
     path('authors/<int:author_id>/', views.author, name='encyc-author'),
     path('contents/', views.contents, name='encyc-contents'),
+    path('sources/<str:source_type>/<int:source_id>/', views.source, name='encyc-source'),
 ]
