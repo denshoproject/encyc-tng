baff41f8e061cd9ed3304ba9bfbda5fee8e1f094
Geoffrey Jost <geoffrey.jost@densho.org>
2024-08-01 08:59:55 -0700

    Add authors list and detail views

--- encyctng/encyclopedia/urls.py
+++ encyctng/encyclopedia/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, re_path
 from encyclopedia import views
 
 urlpatterns = [
+    path('authors/', views.authors, name='encyc-authors'),
+    path('authors/<int:author_id>/', views.author, name='encyc-author'),
     path('contents/', views.contents, name='encyc-contents'),
 ]
--- encyctng/encyclopedia/views.py
+++ encyctng/encyclopedia/views.py
@@ -1,10 +1,25 @@
 from django.http import Http404
 from django.shortcuts import render
 
+from editors.models import Author
 from encyclopedia.models import Article
 
 
 def contents(request):
     return render(request,  'encyclopedia/contents.html', {
         'articles': Article.objects.filter(live=True).order_by('title'),
+    })
+
+def authors(request, template_name='encyclopedia/authors.html'):
+    return render(request, template_name, {
+        'authors': Author.objects.all().order_by('family_name','given_name'),
+    })
+
+def author(request, author_id):
+    # TODO use slug instead of author_id
+    author = Author.objects.get(id=author_id)
+    articles = author.article_set.all()
+    return render(request, 'encyclopedia/author-detail.html', {
+        'author': author,
+        'articles': articles,
     })
