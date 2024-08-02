c99ba3aeb0b68f9c43be59bc40f8f870b1a1d8d1
Geoffrey Jost <geoffrey.jost@densho.org>
2024-08-02 12:06:26 -0700

    Move Source->Article link code from views to models

--- encyctng/encyclopedia/models.py
+++ encyctng/encyclopedia/models.py
@@ -96,6 +96,60 @@ class Article(Page):
     parent_page_types = ['encyclopedia.ArticlesIndexPage']
     subpage_types = []
 
+
+# Article -> sources ---------------------------------------------------
+
+BLOCK_TYPES = ['imageblock','documentblock','videoblock']
+
+BLOCKTYPE_OBJECTTYPE = {
+    'imageblock': 'image',
+    'documentblock': 'document',
+    'videoblock': 'video',
+}
+
+OBJECTTYPE_BLOCKTYPE = {
+    'image': 'imageblock',
+    'document': 'documentblock',
+    'video': 'videoblock',
+}
+
+class ArticleSources():
+    """
+    """
+
+    @staticmethod
+    def link_sources_articles():
+        sources_articles = {'image': {}, 'document': {}, 'video': {}}
+        from encyclopedia.models import Article
+        for article in Article.objects.all():
+            blocks = list(filter(lambda b: b.block_type in BLOCK_TYPES, article.body))
+            for block in blocks:
+                object_type = BLOCKTYPE_OBJECTTYPE[block.block_type]
+                obj = block.value[object_type]
+                sources_articles[object_type][str(obj.id)] = article.id
+                #if block.block_type == 'imageblock':
+                #    image = block.value['image']
+                #    sources_articles['image'][str(image.id)] = article.id
+                #elif block.block_type == 'documentblock':
+                #    document = block.value['document']
+                #    sources_articles['document'][str(document.id)] = article.id
+                #elif block.block_type == 'videoblock':
+                #    video = block.value['video']
+                #    sources_articles['video'][str(video.id)] = article.id
+        return sources_articles
+
+    @staticmethod
+    def source_article_block(source_type, source_id, article):
+        for block in article.body:
+            if BLOCKTYPE_OBJECTTYPE[block.block_type] == source_type:
+                obj = block.value[source_type]
+                if obj.id == source_id:
+                    return block
+        return None
+
+
+# Article footnotes ----------------------------------------------------
+
 ARTICLE_FOOTNOTE_FIELDS = {
     'richtextfields': ['description'],
     'streamfields': ['body'],
--- encyctng/encyclopedia/views.py
+++ encyctng/encyclopedia/views.py
@@ -5,53 +5,13 @@ from wagtail.images.models import Image
 from wagtailmedia.models import Media
 
 from editors.models import Author
-from encyclopedia.models import Article
+from encyclopedia.models import Article, ArticleSources
 
 
 def articles(request):
     return render(request,  'encyclopedia/articles.html', {
         'articles': Article.objects.filter(live=True).order_by('title'),
     })
-
-BLOCK_TYPES = ['imageblock','documentblock','videoblock']
-BLOCKTYPE_OBJECTTYPE = {
-    'imageblock': 'image',
-    'documentblock': 'document',
-    'videoblock': 'video',
-}
-OBJECTTYPE_BLOCKTYPE = {
-    'image': 'imageblock',
-    'document': 'documentblock',
-    'video': 'videoblock',
-}
-
-def link_sources_articles():
-    sources_articles = {'image': {}, 'document': {}, 'video': {}}
-    from encyclopedia.models import Article
-    for article in Article.objects.all():
-        blocks = list(filter(lambda b: b.block_type in BLOCK_TYPES, article.body))
-        for block in blocks:
-            object_type = BLOCKTYPE_OBJECTTYPE[block.block_type]
-            obj = block.value[object_type]
-            sources_articles[object_type][str(obj.id)] = article.id
-            #if block.block_type == 'imageblock':
-            #    image = block.value['image']
-            #    sources_articles['image'][str(image.id)] = article.id
-            #elif block.block_type == 'documentblock':
-            #    document = block.value['document']
-            #    sources_articles['document'][str(document.id)] = article.id
-            #elif block.block_type == 'videoblock':
-            #    video = block.value['video']
-            #    sources_articles['video'][str(video.id)] = article.id
-    return sources_articles
-
-def source_article_block(source_type, source_id, article):
-    for block in article.body:
-        if BLOCKTYPE_OBJECTTYPE[block.block_type] == source_type:
-            obj = block.value[source_type]
-            if obj.id == source_id:
-                return block
-    return None
 
 def source(request, source_type, source_id):
     """
@@ -69,9 +29,9 @@ source = images[0]
     if   source_type == 'image':    source = Image.objects.get(id=source_id)
     elif source_type == 'document': source = Document.objects.get(id=source_id)
     elif source_type == 'video':    source = Media.objects.get(id=source_id)
-    article_id = link_sources_articles()[source_type][str(source.id)]
+    article_id = ArticleSources.link_sources_articles()[source_type][str(source.id)]
     article = Article.objects.get(id=article_id)
-    block = source_article_block(source_type, source_id, article)
+    block = ArticleSources.source_article_block(source_type, source_id, article)
     # IDEA ArticleMedia.metadata(source)
     template = f"encyclopedia/source-{source_type}.html"
     #assert 0
