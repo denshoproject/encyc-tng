639a44d2c171782256988d694b080d0b532fdd66
shyusu4 <shyusupova4@gmail.com>
2025-03-17 18:27:45 +0300

    setup frontend tooling

---
+++ encyctng/static_src/javascript/main.js
@@ -0,0 +1,17 @@
+// IE11 polyfills
+import foreachPolyfill from './polyfills/foreach-polyfill';
+import closestPolyfill from './polyfills/closest-polyfill';
+
+import '../sass/main.scss';
+
+foreachPolyfill();
+closestPolyfill();
+
+function initComponent(ComponentClass) {
+    const items = document.querySelectorAll(ComponentClass.selector());
+    items.forEach((item) => new ComponentClass(item));
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    /* eslint-disable no-new */
+});
