dd7113ba985aec4c85cad5b5b146348ecb33d1f9
Geoffrey Jost <geoffrey.jost@densho.org>
2026-09-16 08:58:43 -0700

    Don't add table-of-contents fields to headings in preview mode
    Our implementation of numbered and nested table-of-contents with
    links to and from headings is based on adding fields to the
    Heading blocks for the numbers and links back to the TOC.
    
    For some weird Wagtail reason this breaks in preview mode.
    Heading blocks contain values that Wagtail doesn't expect to see
    and it freaks out and crashes.
    
    This fix passes `request` to Article.table_of_contents so these
    fields are NOT added in preview mode.

--- encyctng/encyclopedia/templatetags/encyclopedia_tags.py
+++ encyctng/encyclopedia/templatetags/encyclopedia_tags.py
@@ -8,11 +8,11 @@ register = template.Library()
 
 
 @register.simple_tag
-def article_table_of_contents(article):
+def article_table_of_contents(article, request):
     """Generate Table-of-Contents block from article headings"""
     template_name = 'patterns/components/table_of_contents/table_of_contents.html'
     return template.loader.get_template(template_name).render({
-        'table_of_contents': article.table_of_contents(),
+        'table_of_contents': article.table_of_contents(request),
     })
 
 @register.simple_tag
